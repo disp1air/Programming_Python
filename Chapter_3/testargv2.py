7f6b409b5a70f846d45006e322df8390947c7f57
disp1air <hola-shola@mail.ru>
2018-02-16 20:03:06 +0300

    add ch_3 with testargv examples

---
+++ Chapter_3/testargv2.py
@@ -0,0 +1,19 @@
+"собирает параметры командной строки в словаре"
+
+def getopts(argv):
+    opts = {}
+    while argv:
+        if argv[0][0] == '-':           # поиск пар "-name value"
+            opts[argv[0]] = argv[1]     # ключами словарей будут имена параметров
+            argv = argv[2:]
+        else:
+            argv = argv[1:]
+    return opts
+
+if __name__ == '__main__':
+    from sys import argv
+    myargs = getopts(argv)
+    if '-i' in myargs:
+        print(myargs['-i'])
+    print(myargs)
+
