f534fedb240228d6c292a9d8e7d544752ccee196
disp1air <hola-shola@mail.ru>
2018-02-22 21:21:19 +0300

    add biggest_file

--- Chapter_6/biggest_file.py
+++ Chapter_6/biggest_file.py
@@ -2,7 +2,19 @@
 Отыскивает наибольший файл с исходным программным кодом на языке Python
 в единственном каталоге.
 """
-import os, sys, glob
+import os, sys, glob, pprint
 
-dirname = r''
+dirname = r'***'          # path to the directory
+file_extension = '*.js'
+
+list_of_files = glob.glob(dirname + os.sep + file_extension)
+all_sizes = []
+
+for filename in list_of_files:
+    filesize = os.path.getsize(filename)
+    all_sizes.append((filesize, filename))
+
+all_sizes.sort()
+pprint.pprint(all_sizes)
+pprint.pprint(all_sizes[-1])
 
